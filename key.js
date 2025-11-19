711ee38c5a11d39b05bc65fe9c08cfe59544c73d
aiquizfct <aiquizfct@gmail.com>
2025-11-19 19:01:55 +0700

    Add files via upload

---
+++ key.js
@@ -0,0 +1,13 @@
+// Cấu hình Firebase từ Project Chillpromax
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4SGn6J6rrx3VvF6je_QWelhWhyoNfKSA",
+  authDomain: "chillpromax.firebaseapp.com",
+  projectId: "chillpromax",
+  storageBucket: "chillpromax.firebasestorage.app",
+  messagingSenderId: "39810248040",
+  appId: "1:39810248040:web:57e529e64c3d6073c6fadd"
+};
+
+// API Key ImgBB (Dùng để lưu ảnh khi paste vào bài học)
+// Đăng ký miễn phí tại: https://api.imgbb.com/
+export const imgbbApiKey = "bc38f5cce33c55364b687358b9ef7a34";
